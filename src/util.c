ae80d49e2eceeaf988e25b0719dac07a038d7c00
Dustin DuCharme <dustinducharme17@gmail.com>
2024-04-11 10:42:33 -0500

    message

--- src/util.c
+++ src/util.c
@@ -68,12 +68,12 @@ void load_stack(int argc, char* argv[], char*envp[], uint64_t sp_addr)
     int i = 0;
     do
     {
-        data_addr += 1;
         printf("address - 0x%.16lx\n", data_addr);
         byte = read_byte(data_addr);
         printf("%c\n", byte);
-
         read[i] = byte;
+        data_addr += 1;
+
     } while(byte != NULL);
 
     printf("READ FROM MEMORY - %s\n", read);
