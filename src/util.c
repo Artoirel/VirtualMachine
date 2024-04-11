f41dafe36383a76cd6adb646fbf0a9cc7f9be0d3
Dustin DuCharme <dustinducharme17@gmail.com>
2024-04-11 09:36:32 -0500

    message

--- src/util.c
+++ src/util.c
@@ -59,7 +59,7 @@ void load_stack(int argc, char* argv[], char*envp[], uint64_t sp_addr)
     argg[0] = strdup(argv[argc - 2]);
     test = strdup(argv[argc - 2]);
     int num = strlen(argg[0]) + 1;
-    write_block(data_addr, test, num);
+    write_block(data_addr, argg[0], num);
     printf("test\n");
 
     char read[4096];
