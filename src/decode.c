571b2ee5ec99831ee202791bf8c424efa07b3de2
Dustin DuCharme <dustinducharme17@gmail.com>
2024-04-23 07:53:05 -0500

    Test

--- src/decode.c
+++ src/decode.c
@@ -474,7 +474,7 @@ int dispatch(inst_t instruction, uint64_t PC)
                     assert(0 && "RV64_SYS_tee\n");
                 case RV64_SYS_readlinkat:
                     char *fullpath;
-                    fullpath = (get_filiepath(), fullpath);
+                    fullpath = (get_filename(), fullpath);
                     printf("%s\n", fullpath);
                     if(read_reg_long(10) == AT_FDCWD)
                     {
