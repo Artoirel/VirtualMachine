747109873dee4cba1b6cc19f81dee87d56341237
Dustin DuCharme <dustinducharme17@gmail.com>
2024-04-23 06:50:44 -0500

    Test

--- src/decode.c
+++ src/decode.c
@@ -471,6 +471,13 @@ int dispatch(inst_t instruction, uint64_t PC)
                 case RV64_SYS_tee:
                     assert(0 && "RV64_SYS_tee\n");
                 case RV64_SYS_readlinkat:
+                    printf("%d\n", read_reg_long(10));
+                    printf("%d\n", read_reg_long(11));
+                    printf("%d\n", read_reg_long(12));
+                    printf("%d\n", read_reg_long(13));
+                    printf("%d\n", read_reg_long(14));
+                    printf("%d\n", read_reg_long(15));
+                    printf("%d\n", read_reg_long(16));
                     assert(0 && "RV64_SYS_readlinkat\n");
                 case RV64_SYS_fstatat:
                     assert(0 && "RV64_SYS_fstatat\n");
